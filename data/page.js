2f7ce6c906d2fd0c9276ad08bfb0798ccb4f0a00
hexiangdong <670219467@qq.com>
2018-03-02 17:43:43 +0800

    基础结构

---
+++ data/page.js
@@ -0,0 +1,10 @@
+
+var pageData = {
+    "0": [
+        {
+            "pageId": 0,
+            "url": "",
+            "name": "控制Div属性"
+        }
+    ]
+}
---
+++ js/index.js
@@ -0,0 +1,3 @@
+
+console.log(navData)
+console.log(pageData)
